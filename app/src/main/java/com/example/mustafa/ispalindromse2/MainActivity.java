aac6191d1f668a470f0c82cb99a77feb6b124184
Mustafa Zulic <mustafa.zulic93@gmail.com>
2018-03-07 20:22:16 +0100

    Implementierung des Codes in den OnClickListener; Durch klick auf den Button wird das Eingabefeld überprüft ob es sich um ein Palindrom handelt, wenn die Eingabe Leer; kleiner wie 5 ist; oder Leerzeichen hat wird eine Fehlermeldung ausgegeben; Handelt es sich um ein Palindrom kommt die Meldung "True", wenn es sich um kein Palindrom handelt eine Meldung mit "False".

--- app/src/main/java/com/example/mustafa/ispalindromse2/MainActivity.java
+++ app/src/main/java/com/example/mustafa/ispalindromse2/MainActivity.java
@@ -6,38 +6,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText Text;
+    private EditText Eingabe;
     private Button button;
     private boolean test = false;
-
+    private TextView Ausgabe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Text = (EditText)findViewById(R.id.PruefePalindromString);
-
+        Eingabe = (EditText)findViewById(R.id.PruefePalindromString);
+        Ausgabe = (TextView)findViewById(R.id.PalindromAusgabe);
         button = (Button)findViewById(R.id.PruefePalindrom);
 
         button.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
+                if (Eingabe.getText().toString().isEmpty() || Eingabe.getText().toString().equals(" ") || Eingabe.getText().toString().length()<=5 ){
+                    Eingabe.setError("Bitte beachten Sie das die Eingabe nicht kleiner wie 5 Zeichen haben darf, keine Leerzeichen oder Leer ist");
+                }else{
+                    Ausgabe.setText(Boolean.toString(isPalindrome(Eingabe.getText().toString())));
+                }
 
-                test = isPalindrome(Text.getText().toString());
+
             }
         });
-
-        System.out.println(test);
     }
 
     public boolean isPalindrome(String str)	{
-
-        if (str.isEmpty()){
-            Text.setError("Fehlermeldung!!!");
-        }
-       return str.equals(new StringBuilder(str).reverse().toString());
+        return str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
     }
 
 
